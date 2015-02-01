abdcfb3477edf3af37c2d24d98eaaaf4c5ff6950
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-01-31 19:42:13 -0500

    Representation of the board as a string.

--- rushhour.py
+++ rushhour.py
@@ -17,3 +17,23 @@ class RushHour(object):
 
         self.vehicles = vehicles
         self.vehicle_map = {vehicle.id : vehicle for vehicle in vehicles}
+
+    def __repr__(self):
+        s = '-' * 8 + '\n'
+        for line in self.get_board():
+            s += '|{0}|\n'.format(''.join(line))
+        s += '-' * 8 + '\n'
+        return s
+
+    def get_board(self):
+        """Representation of the Rush Hour board as a 2D list of strings"""
+        board = [[' ' for i in range(6)] for j in range(6)]
+        for vehicle in self.vehicles:
+            x, y = vehicle.x, vehicle.y
+            if vehicle.orientation == 'H':
+                for i in range(vehicle.length):
+                    board[y][x+i] = vehicle.id
+            else:
+                for i in range(vehicle.length):
+                    board[y+i][x] = vehicle.id
+        return board
