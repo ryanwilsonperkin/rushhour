fb54cfddae2eb359575ef1664bce69c9b2964d62
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-02 01:13:50 -0500

    Add script for determining difficulty of problem

---
+++ difficulty.py
@@ -0,0 +1,25 @@
+import sys
+import rushhour
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    with open(filename) as rushhour_file:
+        r = rushhour.load_file(rushhour_file)
+
+    results = rushhour.breadth_first_search(r, max_depth=100)
+    solutions = results['solutions']
+    num_solutions = len(solutions)
+
+    if num_solutions == 0:
+        print 'Impossible'
+        sys.exit(1)
+
+    solutions.sort(key=lambda x: len(x))
+    shortest_solution = len(solutions[0])
+
+    if shortest_solution < 20 or num_solutions > 200:
+        print 'Easy'
+    elif shortest_solution > 50 or num_solutions < 20:
+        print 'Hard'
+    else:
+        print 'Medium'
